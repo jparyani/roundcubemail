e086090b99395637b586843e0011a946f5302964
Jason Paryani <github@jparyani.com>
2014-07-29 15:44:53 -0700

    Add a sleep to nginx to wait for php-fpm

--- run_grain.sh
+++ run_grain.sh
@@ -7,7 +7,6 @@ test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
 test -e /var/lock || ln -s /var/run/lock /var/lock
-test -d /var/opt || (mkdir /var/opt && cp -r /opt/app /var/opt/app)
 test -d /var/db || mkdir /var/db
 
 mkdir -p /var/mail
