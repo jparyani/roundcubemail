7de742e7668c7fa2014d29361b5bb9a1146974e4
Jason Paryani <github@jparyani.com>
2015-02-04 13:58:44 -0800

    Updates to get v1.1 working with Sandstorm

--- run_grain.sh
+++ run_grain.sh
@@ -7,7 +7,11 @@ test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
 test -e /var/lock || ln -s /var/run/lock /var/lock
-test -d /var/db || mkdir /var/db
+test -d /var/db || (mkdir /var/db && echo "1.1-rc" > /var/VERSION)
+
+test -e /var/VERSION || echo "1.0" > /var/VERSION
+[[ "$(cat /var/VERSION)" == "1.1-rc" ]] || (cd /opt/app && echo "Upgrading Database...." && ./bin/update.sh && echo "1.1-rc" > /var/VERSION)
+
 
 mkdir -p /var/mail
 touch /var/mail/dovecot-uidlist /var/mail/dovecot-uidvalidity /var/mail/dovecot.index.log
