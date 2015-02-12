2d58d2ebe67109e27df479737aea0ffa53388186
Jason Paryani <github@jparyani.com>
2015-02-12 13:11:41 -0800

    Bump version to v1.1

--- run_grain.sh
+++ run_grain.sh
@@ -7,10 +7,10 @@ test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
 test -e /var/lock || ln -s /var/run/lock /var/lock
-test -d /var/db || (mkdir /var/db && echo "1.1-rc" > /var/VERSION)
+test -d /var/db || (mkdir /var/db && echo "1.1" > /var/VERSION)
 
 test -e /var/VERSION || echo "1.0" > /var/VERSION
-[[ "$(cat /var/VERSION)" == "1.1-rc" ]] || (cd /opt/app && echo "Upgrading Database...." && ./bin/update.sh && echo "1.1-rc" > /var/VERSION)
+[[ "$(cat /var/VERSION)" == "1.1" ]] || (cd /opt/app && echo "Upgrading Database...." && ./bin/update.sh && echo "1.1-rc" > /var/VERSION)
 
 
 mkdir -p /var/mail
