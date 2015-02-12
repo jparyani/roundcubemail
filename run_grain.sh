eefd52ff1011affbdb8928dd2b12053db8558445
Jason Paryani <github@jparyani.com>
2015-02-12 13:36:40 -0800

    Fix version checking

--- run_grain.sh
+++ run_grain.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+CURRENT_VERSION="1.1"
+
 cp -r /etc/service /tmp
 test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
 test -e /var/lock || ln -s /var/run/lock /var/lock
-test -d /var/db || (mkdir /var/db && echo "1.1" > /var/VERSION)
+test -d /var/db || (mkdir /var/db && echo $CURRENT_VERSION > /var/VERSION)
 
 test -e /var/VERSION || echo "1.0" > /var/VERSION
-[[ "$(cat /var/VERSION)" == "1.1" ]] || (cd /opt/app && echo "Upgrading Database...." && ./bin/update.sh && echo "1.1-rc" > /var/VERSION)
+[[ "$(cat /var/VERSION)" == "${CURRENT_VERSION}" ]] || (cd /opt/app && echo "Upgrading Database...." && ./bin/update.sh && echo $CURRENT_VERSION > /var/VERSION)
 
 
 mkdir -p /var/mail
